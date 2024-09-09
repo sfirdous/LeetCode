d04bc304362b8fca7b69cb9d604848f41ff24b81
firdous <sfirdous0404@gmail.com>
2024-09-09 20:18:45 +0530

    Added comments

--- Problem1_MinDistanceToTarget/1848.c
+++ Problem1_MinDistanceToTarget/1848.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int getMinDistance(int* nums, int numsSize, int target, int start) {
+int getMinDistance(int* nums, int numsSize, int target, int start) { //function to get min distance
     int index;
-    for(int i = 0 ; i < numsSize ; ++i){
+    for(int i = 0 ; i < numsSize ; ++i){ //iterating over array element to find the index of target
         if(target == nums[i]){
-            index = i;
-            break;
+            index = i; // when index of target found set it to index variable
+            break;  // get out of the loop when fist occurence of target 
         }
     }
 
-    return abs(index - start);
+    return abs(index - start); // return distance from start to target variable
 }
 
-void accept_array(int *nums,int n){
+void accept_array(int *nums,int n){ // function to accept values array
     printf("\nEnter the values of array: ");
     for (int i = 0; i < n; i++){
         scanf("%d",&nums[i]);
@@ -25,16 +25,16 @@ int main()
 {
     int n;
     printf("\nEnter size of array: ");
-    scanf("%d",&n);
+    scanf("%d",&n); // accepting size of array from user
 
-    int *array = calloc(n,sizeof(n));
-    accept_array(array,n);
+    int *array = calloc(n,sizeof(n)); // dynamically allocating the array
+    accept_array(array,n); // accepting array values from user
 
-    int target,start;
+    int target,start; 
     printf("\nEnter target and start: ");
-    scanf("%d%d",&target,&start);
+    scanf("%d%d",&target,&start); // accepting target and start value from user
 
-    int distance = getMinDistance(array,n,target,start);
-    printf("\n%d",distance);
+    int distance = getMinDistance(array,n,target,start); // caluculating the minimum distance
+    printf("\n%d",distance); // display the min distance
 
 }
