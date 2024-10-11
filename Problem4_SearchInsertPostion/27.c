f57a27cc2302da4dd16853cc3f35bd5dd67e5f09
firdous <sfirdous0404@gmail.com>
2024-10-11 12:58:03 +0530

    Added comments

--- Problem4_SearchInsertPostion/27.c
+++ Problem4_SearchInsertPostion/27.c
@@ -1,25 +1,23 @@
 
 int searchInsert(int* nums, int numsSize, int target) {
     
-    int begin = 0,end = numsSize-1;
-    int   mid;
+    int begin = 0,end = numsSize-1; //assign begin and end values
+    int   mid; //declare a variable to caluculate middle value
    
     while(begin<end)
     {
-        mid = (begin+end)/2;
-        // printf("%d\n",mid);
+        mid = (begin+end)/2; //calculate mid index for each part
         if(nums[mid] == target){
-            printf("found\n");
-            return mid;
+            return mid; //return mid if target found
 
         }
-        if(nums[mid] < target){
+        if(nums[mid] < target){ //if mid value is smaller target is present in the lower part 
             begin = mid + 1;
         }else{
-            end = mid - 1;
+            end = mid - 1; //else target is present in upper half
         }
     
     }
 
-    return begin+1;
+    return begin+1; //if the element is not in the array it is present at this index except for [1,3,5,7] and target = 7
 }
