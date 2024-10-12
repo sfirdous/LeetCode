fb590b17ea1c43bcaafc9391bccede6935bd8b69
firdous <sfirdous0404@gmail.com>
2024-10-12 21:55:31 +0530

    Added main function

--- Problem3_RemoveElement/27.c
+++ Problem3_RemoveElement/27.c
@@ -11,3 +11,33 @@ int removeElement(int* nums, int numsSize, int val) {
     }
     return index;  // The new size of the array without `val`
 }
+
+void accept_array(int *nums,int n){ // function to accept values array
+    printf("\nEnter the values of array: ");
+    for (int i = 0; i < n; i++){
+        scanf("%d",&nums[i]);
+    }
+    
+}
+
+void print_array(int *nums,int n){ // function to print values array
+    printf("\nValues of array: ");
+    for (int i = 0; i < n; i++){
+        printf("%d\t",nums[i]);
+    }
+    
+}
+int main()
+{
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d",&n);
+    int *nums = calloc(n,sizeof(n));
+    accept_array(nums,n);
+    int val;
+    printf("Enter the value to be removed: ");
+    scanf("%d",&val);
+
+    removeElement(nums,n,val);
+    print_array(nums,n);
+}
