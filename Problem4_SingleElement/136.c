497c4044ab743f3b397b67e38a2e88a2ccb6e26f
firdous <sfirdous0404@gmail.com>
2024-10-16 20:49:46 +0530

    Solved 136 problem partially

---
+++ Problem4_SingleElement/136.c
@@ -0,0 +1,15 @@
+int singleNumber(int* nums, int numsSize) {
+
+    if(numsSize>1){
+    int single = 0;
+    for(int i = 1 ; i < numsSize;++i)
+    {
+        if(nums[single] == nums[i]){
+            single++;
+        }
+    }  
+
+    return nums[single]; 
+    }
+    return nums[0];
+}
