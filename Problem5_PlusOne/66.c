c57f5b9731ef11d77a7af4c47bb181d3a265b393
firdous <sfirdous0404@gmail.com>
2024-10-31 19:20:41 +0530

    Did some changes

--- Problem5_PlusOne/66.c
+++ Problem5_PlusOne/66.c
@@ -1,9 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* plusOne(int* digits, int digitsSize, int returnSize) {
+#include<stdio.h>
+#include<stdlib.h>
+
+void print_array(int *nums,int n){ // function to print values array
+    printf("\nValues of array: ");
+    for (int i = 0; i < n; i++){
+        printf("%d\t",nums[i]);
+    }
+    
+}
+
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
    int number = 0;
    for(int i = 0;i<digitsSize;++i){
     number = number*10 + digits[i]; 
@@ -11,23 +20,50 @@ int* plusOne(int* digits, int digitsSize, int returnSize) {
 
    number += 1;
    int number2 = number;
-   returnSize = 0;
+    int count =0 ;
    while(number2!=0){
     number2/=10;
-    returnSize++;
+    count++;
    }
 
-   int *rPlusDigits = malloc(returnSize*sizeof(int));
-   int *plusDigits = malloc(returnSize*sizeof(int));
-   for(int i = 0 ; i < returnSize;++i){
-        rPlusDigits[i] = number%10;
+   int *plusDigits = malloc(count*sizeof(int));
+   for(int i = count; i >= 0;--i){
+        plusDigits[i] = number%10;
         number /= 10; 
    }
 
-    int j;
-   for(int i = 0,j = returnSize -1;j>=0,i<returnSize;++i,--j){
-    plusDigits[i] = rPlusDigits[j];
-   }
+    *returnSize = count;
 
    return plusDigits;
 }
+
+
+int main()
+{
+
+    int numsSize = 3;
+    int *nums = malloc(numsSize*sizeof(int));
+
+    for (int i = 0; i < numsSize; i++)
+    {
+        scanf("%d",&nums[i]);
+    }
+    
+    
+
+    int resultSize;
+
+    int *result = plusOne(nums,numsSize,&resultSize);
+    print_array(result,resultSize);
+
+    
+
+    // Free the dynamically allocated memory for result
+    free(result);
+
+    return 0;
+
+
+}
+
+
