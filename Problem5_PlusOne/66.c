f0da689c8236122b1f37cbc23842c22e3842b31b
firdous <sfirdous0404@gmail.com>
2024-11-02 18:59:53 +0530

    Works for array whose last element is less than 9

--- Problem5_PlusOne/66.c
+++ Problem5_PlusOne/66.c
@@ -14,63 +14,39 @@ void print_array(int *nums,int n){ // function to print values array
 
 //runtime error
 int* plusOne(int* digits, int digitsSize, int* returnSize) {
-   int number = 0;
-   for(int i = 0;i<digitsSize;++i){
-    number = number*10 + digits[i]; 
-    printf("number: %d\n",number);
-   }
-
-   number += 1;
-   printf("number: %d\n",number);
-   int number2 = number;
-    printf("number2: %d\n",number2);
-    int count = 0 ;
-   while(number2!=0){
-    number2/=10;
-    count++;
-    printf("count: %d\n",count);
-   }
-
-   printf("count: %d\n",count);
-   int *plusDigits = malloc(count*sizeof(int));
-   for(int i = count-1; i >= 0;--i){
-        plusDigits[i] = number%10;
-         printf("plusDigits[%d]: %d\n",i,plusDigits[i]);
-        number /= 10; 
-   }
-
-    *returnSize = count;
-
-   return plusDigits;
+    
+    int *plusOne;
+    if(digits[digitsSize-1]!=9){
+        plusOne = calloc(digitsSize,sizeof(int));
+        for (int i = 0; i < digitsSize; i++)
+        {
+            if(i == (digitsSize-1))
+                plusOne[i] = 1+digits[i];
+            else
+                plusOne[i] = digits[i];
+        }
+        
+        *returnSize = digitsSize;
+        return plusOne;
+    }
 }
 
 
 int main()
 {
-
-    int numsSize = 1;
+    int numsSize;
+    printf("Enter the size of array: ");
+    scanf("%d",&numsSize);
     int *nums = malloc(numsSize*sizeof(int));
-
+    printf("Enter the elements of array: ");
     for (int i = 0; i < numsSize; i++)
     {
         scanf("%d",&nums[i]);
     }
-    
-    
-
     int resultSize;
-
     int *result = plusOne(nums,numsSize,&resultSize);
     print_array(result,resultSize);
-
-    
-
     // Free the dynamically allocated memory for result
     free(result);
-
     return 0;
-
-
 }
-
-
