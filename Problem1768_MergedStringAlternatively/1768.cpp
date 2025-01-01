1aee6e7e00a574f7f6160dd2d804ad425109a905
firdous <sfirdous0404@gmail.com>
2025-01-01 19:39:17 +0530

    Mofified problem 1768 of leetcode

--- Problem1768_MergedStringAlternatively/1768.cpp
+++ Problem1768_MergedStringAlternatively/1768.cpp
@@ -3,16 +3,16 @@ public:
     string mergeAlternately(string word1, string word2) {
         int w1_s = word1.size();
         int w2_s = word2.size();
-        int length = w1_s+w2_s;
+        int length = (w1_s>w2_s) ? w1_s : w2_s;
         string merged;
-        int w1=0,w2=0;
-        for(int i = 0 ; i <= length;++i){
-            if(i%2 == 0 && w1 < w1_s ){
-                merged.push_back(word1[w1]);
-                w1++;
-            }else if(w2 < w2_s){
-                merged.push_back(word2[w2]);
-                w2++;
+        
+        for(int i = 0 ; i < length;++i){
+            if(i < w1_s ){
+                merged += word1[i];
+                
+            } if(i < w2_s){
+                merged+=word2[i];
+              
             }
         }
         return merged;
