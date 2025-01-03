781c5293045837188a85a6cb9b79180da62dad7a
firdous <sfirdous0404@gmail.com>
2025-01-03 13:12:08 +0530

    Solved 80/80 testcases

--- Problem1732_HighestAltitude/1732.cpp
+++ Problem1732_HighestAltitude/1732.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int length = gain.size();
-        vector<int> altitudes(length+1,0);
-        for(int i = 0 ; i < length;++i){
-            altitudes[i+1] = altitudes[i] + gain[i];
-        }
-        
-        int max = altitudes[0];
-         for(int i = 0 ; i < length;++i){
-            if(altitudes[i] > max){
-                max = altitudes[i];
+      int max = 0;
+      int new_max;
+      for(int i = 0 ; i < gain.size();++i){
+            new_max += gain[i];
+            if(new_max>max){
+                max = new_max;
             }
-        }
-
-
-        
+      }  
         return max;
     }
 };
