b7980bfb5f48d508d70f56bc60603fb5ee9bd075
firdous <sfirdous0404@gmail.com>
2025-01-06 06:56:12 +0530

    Solved 217 of Leetcode

---
+++ Problem217_ContainsDuplicate/217.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int size = nums.size();
+        int temp;
+        sort(nums.begin(),nums.end());
+        for(int i = 1 ; i < size;++i){
+            if(nums[i] == nums[i-1]){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
