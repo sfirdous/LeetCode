dfb20a7ea3b857d6eb6c563c8464f9d1402b87c9
firdous <sfirdous0404@gmail.com>
2025-02-13 21:04:45 +0530

    Solved problem 724 of Leetcode

---
+++ Problem724_FindPivotIndex/724.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int totalSum = 0;
+        int leftSum = 0;
+
+        for(int n : nums){
+            totalSum += n;
+        }
+
+        for(int i = 0; i < n;++i ){
+            if(leftSum == (totalSum - leftSum - nums[i])){
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
