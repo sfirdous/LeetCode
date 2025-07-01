f95d21c6df5e3d387e1d6da3c3c06576d2ccb69b
firdous <sfirdous0404@gmail.com>
2025-07-01 22:55:07 +0530

    Solved problem 435

---
+++ LeetCode75/Intervals/Problem435_Non-overlappingIntervals/435.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+       if (intervals.empty())
+            return 0;
+
+        // Sort intervals by end time
+        sort(intervals.begin(), intervals.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 return a[1] < b[1];
+             });
+
+        int count = 0;
+        int prevEnd = intervals[0][1];
+
+        for (int i = 1; i < intervals.size(); ++i)
+            (intervals[i][0] < prevEnd) ? ++count : prevEnd = intervals[i][1];
+
+        return count;
+    }
+};
