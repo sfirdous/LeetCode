011f1a714256600cc9ca4f4fd9ac1934ae20a129
firdous <sfirdous0404@gmail.com>
2025-07-30 21:35:34 +0530

    Solved task 610

---
+++ Miscellaneous/SQL/610TriangleJudgement.sql
@@ -0,0 +1,12 @@
+select x,y,z,
+CASE 
+    when x+ y > z and y + z > x and x+z > y then 'Yes'
+    else 'No'
+END as triangle 
+from Triangle
+select x,y,z,
+CASE 
+    when x+ y > z and y + z > x and x+z > y then 'Yes'
+    else 'No'
+END as triangle 
+from Triangle
